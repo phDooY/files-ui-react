e782495d3d2f20f1a8aa8828d826db415d23de8a
phDooY <david91.05@mail.ru>
2024-12-21 14:35:52 +0200

    chore: types included

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,7 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import typescript from "rollup-plugin-typescript2";
 import sass from "rollup-plugin-sass";
 import packageJson from "./package.json";
-//import jsx from 'rollup-plugin-jsx'
-//import json from "rollup-plugin-json";
-//import { terser } from "rollup-plugin-terser";
+
 export default {
   input: "./src/index.ts",
   output: [
@@ -23,15 +21,20 @@ export default {
   ],
   plugins: [
     sass({ insert: true }),
-    
     peerDepsExternal(),
-    resolve(
-     // { preferBuiltins: true, mainFields: ['browser'] }
-      ),
+    resolve(),
     commonjs(),
-    typescript(),
-    //terser(),
-    //json(),
-    // jsx( {factory: 'React.createElement'} ),
+    typescript({
+      useTsconfigDeclarationDir: true,
+      tsconfigOverride: {
+        compilerOptions: {
+          declaration: true,
+          declarationDir: "build",
+          emitDeclarationOnly: false
+        },
+        exclude: ["**/*.test.tsx", "**/*.stories.tsx", "node_modules"]
+      }
+    })
   ],
+  external: ["react", "react-dom"]
 };
